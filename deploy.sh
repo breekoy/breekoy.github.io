0a845a865e16ae296d037c07ef3c1cb732fbdcab
Bree Dingding <breedingding@coredev.ph>
2020-04-23 00:10:58 +0800

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:breekoy/breekoy.github.io.git master:gh-pages
+git push -f git@github.com:breekoy/breekoy.github.io.git redesign:master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:breekoy/countdown.git master:gh-pages
